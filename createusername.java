ce132f7302824388fa72410831dddddca112b723
NandiniVeerabadriya26 <nandiniveerabadriya@gmail.com>
2025-01-03 09:55:59 +0530

    Add files via upload

---
+++ createusername.java
@@ -0,0 +1,10 @@
+public class DemoSystem{
+    public static void main(String[] args) {
+        String Username = "user_001";
+        String Password = "password123";
+        while(username == "user_001") && (password == "password123"){
+            System.out.println("Hello Happy New year");
+            break;
+        }
+    }
+}
