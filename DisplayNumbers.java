b8743df3fe6b6641031168dd90e62db5c1cd63df
NandiniVeerabadriya26 <nandiniveerabadriya@gmail.com>
2025-01-03 10:03:43 +0530

    Add files via upload

---
+++ DisplayNumbers.java
@@ -0,0 +1,8 @@
+public class DisplayNumbers {
+    public static void main(String[] args) {
+        // Display numbers from 1 to 100 using a for loop
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
